19a3f64ed6bfdf52af8dcc930efda78ecd58228d
K-1303 <kavyadua03@gmail.com>
2025-09-04 23:45:10 +0530

    Implemented mongoDB odm and defined schema for all data types.

--- llm_engineering/settings.py
+++ llm_engineering/settings.py
@@ -4,3 +4,7 @@ class Settings(BaseSettings):
     # LinkedIn Credentials
     LINKEDIN_USERNAME: str | None = None
     LINKEDIN_PASSWORD: str | None = None
+
+    # MongoDB Configuration
+    DATABASE_NAME: str = "llm_twin"
+    DATABASE_HOST: str = "mongodb://localhost:27017"
